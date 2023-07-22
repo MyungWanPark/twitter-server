21e6d865788425d73f8999fd8065dbea98e74e16
MyungWan Park <mywanpark@gmail.com>
2023-07-22 23:12:44 +0900

    Chore: csrf 토큰
    - csrf 토큰값을 구하기 위한 함수 구현

--- middleware/csrf.js
+++ middleware/csrf.js
@@ -1,21 +1,27 @@
-import bcrypt from 'bcrypt';
-import { config } from '../config.js';
+import bcrypt from "bcrypt";
+import { config } from "../config.js";
 
 export const csrfCheck = (req, res, next) => {
   if (
-    req.method === 'GET' ||
-    req.method === 'OPTIONS' ||
-    req.method === 'HEAD'
+    req.method === "GET" ||
+    req.method === "OPTIONS" ||
+    req.method === "HEAD"
   ) {
     return next();
   }
 
-  const csrfHeader = req.get('twitter-csrf-token');
+  const csrfHeader = req.get("twitter-csrf-token");
 
   if (!csrfHeader) {
     console.warn(`Missing required twitter csrf header ${req.headers.origin}`);
-    return res.status(403).json({ message: 'Failed CSRF check' });
+    return res.status(403).json({ message: "Failed CSRF check" });
   }
+  const getCSRFToken = async () => {
+    const token = await bcrypt.hash(csrfHeader, 12);
+    console.log("bycrpt token = ", token);
+  };
+
+  // getCSRFToken();
 
   validateCsrfToken(csrfHeader) //
     .then((isValid) => {
@@ -23,13 +29,13 @@ export const csrfCheck = (req, res, next) => {
         console.warn(
           `Value provided in twitter csrf token header(${csrfHeader}) does not validate ${req.headers.origin}`
         );
-        return res.status(403).json({ message: 'Failed CSRF check' });
+        return res.status(403).json({ message: "Failed CSRF check" });
       }
       next();
     })
     .catch((err) => {
       console.log(err);
-      return res.status(500).json({ message: 'Something went wrong' });
+      return res.status(500).json({ message: "Something went wrong" });
     });
 };
 
